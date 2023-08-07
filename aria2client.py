ffe7a564b0eae2a4b24da4bb06bf0251f72036c0
bianca2190 <109406227+bianca2190@users.noreply.github.com>
2023-08-07 12:30:22 +0700

    Update aria2client.py

--- aria2client.py
+++ aria2client.py
@@ -27,7 +27,7 @@ class Aria2Client:
                                                                            loads=ujson.loads,
                                                                            dumps=ujson.dumps, )
 
-        # 先取消回调
+        # Batalkan panggilan balik terlebih dahulu
         # self.client.unregister(self.on_download_start, "aria2.onDownloadStart")
         # self.client.unregister(self.on_download_pause, "aria2.onDownloadPause")
         # self.client.unregister(self.on_download_complete, "aria2.onDownloadComplete")
@@ -35,12 +35,12 @@ class Aria2Client:
 
 
     async def on_download_start(self, trigger, data):
-        print(f"===========下载 开始 {data}")
+        print(f"===========unduh mulai {data}")
         gid = data['params'][0]['gid']
-        # 查询是否是绑定特征值的文件
+        # Kueri apakah itu file dengan nilai fitur terikat
         tellStatus = await self.client.tellStatus(gid)
         await self.bot.send_message(SEND_ID,
-                                    f'{getFileName(tellStatus)} 任务已经开始下载... \n 对应路径: {tellStatus["dir"]}',
+                                    f'{getFileName(tellStatus)} Tugas telah mulai diunduh... \n jalur yang sesuai: {tellStatus["dir"]}',
                                     parse_mode='html')
 
     async def on_download_pause(self, trigger, data):
@@ -48,45 +48,45 @@ class Aria2Client:
 
         tellStatus = await self.client.tellStatus(gid)
         filename = getFileName(tellStatus)
-        print('回调===>任务: ', filename, '暂停')
-        # await bot.send_message(SEND_ID, filename + ' 任务已经成功暂停')
+        print('panggilan balik ===> tugas: ', filename, 'berhenti sebentar')
+        # await bot.send_message(SEND_ID, filename + ' Tugas telah berhasil dijeda')
 
     async def on_download_complete(self, trigger, data):
-        print(f"===========下载 完成 {data}")
+        print(f"===========Pengunduhan selesai {data}")
         gid = data['params'][0]['gid']
 
         tellStatus = await self.client.tellStatus(gid)
         files = tellStatus['files']
-        # 上传文件
+        # unggah berkas
         for file in files:
             path = file['path']
             await self.bot.send_message(SEND_ID,
-                                        '下载完成===> ' + path,
+                                        'Pengunduhan selesai===> ' + path,
                                         )
-            # 发送文件下载成功的信息
+            # Kirim pesan bahwa file berhasil diunduh
 
             if UP_TELEGRAM:
                 if '[METADATA]' in path:
                     os.unlink(path)
                     return
-                print('开始上传,路径文件:' + path)
+                print('mulai mengunggah, file jalur:' + path)
                 msg = await self.bot.send_message(SEND_ID,
-                                                  '上传中===> ' + path,
+                                                  'mengunggah===> ' + path,
                                                   )
 
                 async def callback(current, total):
-                    # print("\r", '正在发送', current, 'out of', total,
+                    # print("\r", 'mengirim', current, 'out of', total,
                     #       'bytes: {:.2%}'.format(current / total), end="", flush=True)
-                    # await bot.edit_message(msg, path + ' \n上传中 : {:.2%}'.format(current / total))
+                    # await bot.edit_message(msg, path + ' \nmengunggah : {:.2%}'.format(current / total))
                     print(current / total)
 
                 try:
-                    # 单独处理mp4上传
+                    # Tangani unggahan mp4 secara terpisah
                     if '.mp4' in path:
 
                         pat, filename = os.path.split(path)
                         await order_moov(path, pat + '/' + 'mo-' + filename)
-                        # 截图
+                        # tangkapan layar
                         await imgCoverFromFile(path, pat + '/' + filename + '.jpg')
                         os.unlink(path)
                         await self.bot.send_file(SEND_ID,
@@ -107,19 +107,20 @@ class Aria2Client:
                         os.unlink(path)
 
                 except FileNotFoundError as e:
-                    print('文件未找到')
+                    print('berkas tidak ditemukan')
 
     async def on_download_error(self, trigger, data):
-        print(f"===========下载 错误 {data}")
+        print(f"===========kesalahan unduhan {data}")
         gid = data['params'][0]['gid']
 
         tellStatus = await self.client.tellStatus(gid)
         errorCode = tellStatus['errorCode']
         errorMessage = tellStatus['errorMessage']
-        print('任务', gid, '错误码', errorCode, '错误信息：', errorMessage)
+        print('Tugas', gid, 'kode kesalahan', errorCode, 'pesan eror：', errorMessage)
         if errorCode == '12':
-            await self.bot.send_message(SEND_ID, ' 任务正在下载,请删除后再尝试')
+            await self.bot.send_message(SEND_ID, ' Tugas sedang mengunduh, harap hapus dan coba lagi')
         else:
             await self.bot.send_message(SEND_ID, errorMessage)
 
         pprint(tellStatus)
+        
